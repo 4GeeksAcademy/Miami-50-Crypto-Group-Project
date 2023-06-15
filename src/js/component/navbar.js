c475c1d8efbdc17cbb884f8898ba06e9547cd075
David Avery <124400470+averydi@users.noreply.github.com>
2023-06-15 18:41:31 +0000

    CSS additions

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -1,11 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import './navbar.css';
 
 export const Navbar = () => {
 	return (
 		<nav className="navbar navbar-light bg-light mb-3">
 			<Link to="/">
-				<span className="navbar-brand mb-0 h1">React Boilerplate</span>
+				<span className="navbar-brand mb-0 h1">Crypto App</span>
 			</Link>
 			<div className="ml-auto">
 				<Link to="/Login">
